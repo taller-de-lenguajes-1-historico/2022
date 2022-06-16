cbfcc08670a143846da7f6cec0d5066f8360fbef
Javier <javiergrana@hotmail.com>
2022-06-15 22:44:49 -0300

    Agregando API Resto al proyecto

---
+++ Clase8/ObjetoDirectory/CosumiendoAPIRest/Program.cs
@@ -0,0 +1,91 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+
+namespace TrabajandoJson
+{
+    partial class Program
+    {
+
+        //https://json2csharp.com/
+
+        static void Main(string[] args)
+        {
+            GetProvinciasArgentinas();
+            GetClima();
+        }
+
+
+        private static void GetProvinciasArgentinas()
+        {
+            var url = $"https://apis.datos.gob.ar/georef/api/provincias?campos=id,nombre";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        if (strReader == null) return;
+                        using (StreamReader objReader = new StreamReader(strReader))
+                        {
+                            string responseBody = objReader.ReadToEnd();
+                            ProvinciasArgentina ListProvincias = JsonSerializer.Deserialize<ProvinciasArgentina>(responseBody);
+                            foreach (Provincia Prov in ListProvincias.Provincias)
+                            {
+                                Console.WriteLine("id: " + Prov.Id + " Nombre: " + Prov.Nombre);
+                            }
+
+                        }
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine("Problemas de acceso a la API");
+            }
+        }
+
+        private static void GetClima()
+        {
+           var url = $"https://ws.smn.gob.ar/map_items/weather/";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+           
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        if (strReader == null) return;
+                        using (StreamReader objReader = new StreamReader(strReader))
+                        {
+                            string responseBody = objReader.ReadToEnd();
+                            List<Root> listclima = JsonSerializer.Deserialize<List<Root>>(responseBody);
+                            foreach (var Prov in listclima)
+                            {
+                                Console.WriteLine("Nombre: " + Prov.name + " Temperatura: " + Prov.weather.temp);
+                            }
+
+                        }
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine("Problemas de acceso a la API");
+            }
+        }
+
+    }
+}
